a76f7a5aaf72146e899f72fc561ba7924d7b1ed2
Dagem Tsehay <dagemtsehay4@gmail.com>
2023-08-22 12:04:27 +0300

    disabled ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
-import HomePage from "@/features/pages/home";
+// import HomePage from "@/features/pages/home";
+import dynamic from 'next/dynamic'
+const HomePage = dynamic(() => import('@/features/pages/home'), {
+  ssr: false
+})
 export default function Home() {
   return <HomePage/>
 }
